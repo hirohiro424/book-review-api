95725edf17692d549a6f2f80cbf34fd0976db008
wonjin <swj6824@naver.com>
2025-07-31 13:26:21 +0900

    user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'nickname', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('개인 정보', {'fields': ('nickname',)}),
+        ('권한 설정', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('중요 날짜', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'nickname')
+    ordering = ('email',)
+
+admin.site.register(User, UserAdmin)
